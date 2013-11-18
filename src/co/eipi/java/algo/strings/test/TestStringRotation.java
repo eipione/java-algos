eb9fca86d036ecaa0834f8ebf51f7efa566cada6
eipione <misam@Misams-MacBook-Air.local>
2013-11-18 19:32:39 +0530

    String Rotation

---
+++ src/co/eipi/java/algo/strings/test/TestStringRotation.java
@@ -0,0 +1,75 @@
+package co.eipi.java.algo.strings.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import co.eipi.java.algo.strings.StringRotation;
+import co.eipi.java.algo.test.TestUtils;
+
+public class TestStringRotation
+{
+
+    @Test
+    public void testRotateTrue()
+    {
+	int size = 100;
+
+	List<String> original = TestUtils.genRandomStringList(size, 10);
+	List<String> rotated = new ArrayList<String>(size);
+
+	for (String orig : original)
+	{
+	    rotated.add(TestUtils.rotateString(orig));
+	}
+
+	System.out.println(original);
+	System.out.println(rotated);
+
+	for (int i = 0; i < size; i++)
+	{
+	    Assert.assertTrue("Rotation Test Failed for " + original.get(i)
+		    + "," + rotated.get(i), StringRotation.isRotationOf(
+		    original.get(i), rotated.get(i)));
+	}
+
+	Assert.assertTrue("Rotation Positive Test Failed for " + null + "," + null,
+		StringRotation.isRotationOf(null, null));
+
+    }
+
+    @Test
+    public void testRotateFalse()
+    {
+
+	List<String> original = new ArrayList<String>();
+	List<String> rotated = new ArrayList<String>();
+
+	original.add(null);
+	rotated.add("");
+
+	original.add(null);
+	rotated.add("ok");
+
+	original.add("");
+	rotated.add("ok");
+
+	original.add("abc");
+	rotated.add("abcd");
+
+	original.add("abc");
+	rotated.add("cba");
+
+	System.out.println(original);
+	System.out.println(rotated);
+
+	for (int i = 0; i < original.size(); i++)
+	{
+	    Assert.assertFalse("Rotation Negative Test Failed for " + original.get(i)
+		    + "," + rotated.get(i), StringRotation.isRotationOf(
+		    original.get(i), rotated.get(i)));
+	}
+    }
+}
--- src/co/eipi/java/algo/test/TestUtils.java
+++ src/co/eipi/java/algo/test/TestUtils.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class TestUtils
 {
     
@@ -25,5 +27,38 @@ public class TestUtils
 	}
 	
 	return retList;
+    }
+    
+    /**
+     * Generates a random string array with length up to maxLength
+     * @param size - the size of the list
+     * @param maxLength - the maximum length of the string
+     * @return
+     */
+    public static List<String> genRandomStringList(int size,int maxLength)
+    {
+	List<String> retList = new ArrayList<String>(size);
+	Random random = new Random();
+	for (int i=0;i<size;i++)
+	{
+	    retList.add(RandomStringUtils.randomAlphabetic(random.nextInt(maxLength)));
+	}
+		
+	return retList;
+    }
+    
+    public static String rotateString(String in)
+    {
+	if (in.length() == 0) return in;
+	Random random = new Random();
+	int randomStart = random.nextInt(in.length());
+	char[] chars = new char[in.length()];
+	
+	for(int i=randomStart;i<randomStart+in.length();i++)
+	{
+	    chars[i-randomStart] = in.charAt(i%in.length());
+	}
+	
+	return new String(chars);
     }
 }
